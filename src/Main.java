c15e9ae49eb6f81cadf874a7a7049bb715140c07
HunterYarmy <huntery5@nycstudents.net>
2025-11-18 15:34:26 -0500

    reset the project and used Mr. Holmers thingy

--- src/Main.java
+++ src/Main.java
@@ -11,56 +11,5 @@ public class Main {
         File f = new File("Scoreboard.txt");
         Scanner s = new Scanner(f);
 
-        Team[] teams = new Team[7];
 
-        while (s.hasNext()) {
-
-            if (!s.hasNextInt()) {
-                String teamOne = s.next();
-                String teamTwo = s.next();
-
-                for (int i = 0; i < 7; i++){
-                    if (!teams[i].equals(teamOne) & teams[i] == null) {
-
-                        Team temp = new Team(teamOne);
-                        teams[i] = temp;
-
-                        Team t1 = teams[i];
-                    }
-                    else if (teams[i].equals(teamOne))
-                        Team t1 = teams[i];
-
-                    if (!teams[i].equals(teamTwo) & teams[i] == null) {
-
-                        Team temp = new Team(teamTwo);
-                        teams[i] = temp;
-
-                        Team t2 = teams[i];
-                    }
-                    else if (teams[i].equals(teamTwo))
-                        Team t2 = teams[i];
-
-                }
-
-            }
-
-            int[] scores = new int[2];
-            int x = 0;
-
-            while (s.hasNextInt()) {
-                if (s.nextInt() == 0) {
-                    if (x == 0) x++;
-                    else x--;
-                }
-                scores[x] += s.nextInt();
-            }
-
-            t1.addPoints(scores[0]);
-            t2.addPoints(scores[1]);
-
-            
-
-        }
-
-    }
 }
---
+++ src/Scoreboard.java
@@ -0,0 +1,17 @@
+public class Scoreboard {
+
+    private String team1;
+    private String team2;
+
+    private int score1;
+    private int score2;
+
+    public Scoreboard(String team1, String team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+    }
+
+    public void recordPlay(int points) {
+
+    }
+}
