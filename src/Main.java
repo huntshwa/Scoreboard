2931937cfb959225e5e60bc9d9fba535e0747b78
HunterYarmy <huntery5@nycstudents.net>
2025-11-20 15:34:48 -0500

    added team class and updated main and finshed scoreboard class

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,15 @@ public class Main {
     public static String read() throws FileNotFoundException {
         File f = new File("Scoreboard.txt");
         Scanner s = new Scanner(f);
+        Scoreboard game;
+
+        while (s.hasNext()) {
+            if (!s.hasNextInt()) game = new Scoreboard(s.next(), s.next());
+
+            while (s.hasNextInt()) {
+                game.recordPlay(s.nextInt());
+            }
 
 
+        }
 }
--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -6,12 +6,37 @@ public class Scoreboard {
     private int score1;
     private int score2;
 
+    private boolean activeTeam;
+
     public Scoreboard(String team1, String team2) {
         this.team1 = team1;
         this.team2 = team2;
+        activeTeam = true;
     }
 
     public void recordPlay(int points) {
+        if (points != 0)
+            if (activeTeam) score1 += points;
+            else score2 += points;
 
+        else activeTeam = !activeTeam;
+    }
+
+    public String getScore() {
+        String team;
+
+        if (activeTeam) team = team1;
+        else team = team2;
+
+        return score1 + "-" + score2 + "-" + team;
+    }
+
+    public String getWin() {
+        String team;
+
+        if (activeTeam) team = team1;
+        else team = team2;
+
+        return team;
     }
 }
